ae6b213461df6b4af52e39eac7b837a886a13a51
devjahid1 <developerjahid1@gmail.com>
2025-03-10 03:12:12 +0600

    done

--- Variable-j1.js
+++ Variable-j1.js
@@ -4,12 +4,5 @@ const students = 17;
 const marks = 390;
 const rooms = 6;
 const study = 9;
-const total = 0+1+6+0+6+4+4+0+8+1+9;
-console.log(birthYear);
-console.log(brothers);
-console.log(students);
-console.log(marks);
-console.log(rooms);
-console.log(study);
-console.log(total);
+const total = 39;
 
