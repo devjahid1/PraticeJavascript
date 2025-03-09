f974b641372bf4196abab3e247d99aec18ad8924
devjahid1 <developerjahid1@gmail.com>
2025-03-10 03:08:17 +0600

    code done

---
+++ script.js
@@ -0,0 +1,15 @@
+const birthYear = 2002;
+const brothers = 2;
+const students = 17;
+const marks = 390;
+const rooms = 6;
+const study = 9;
+const total = 0+1+6+0+6+4+4+0+8+1+9;
+console.log(birthYear);
+console.log(brothers);
+console.log(students);
+console.log(marks);
+console.log(rooms);
+console.log(study);
+console.log(total);
+
