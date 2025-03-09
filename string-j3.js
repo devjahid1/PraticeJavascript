1f95d7e759b8806a0bef678aca1855687e0d7fd2
devjahid1 <developerjahid1@gmail.com>
2025-03-10 03:29:09 +0600

    string done

---
+++ string-j3.js
@@ -0,0 +1,20 @@
+const name = 'Md. Jahid Miah Suyeb';
+console.log(name);
+const address = 'Kajalshah, Sylhet';
+console.log(address);
+const versity = 'Metropolitan University';
+console.log(versity);
+const fan = 'Habiba';
+console.log(fan);
+const movieOfTheYear = 'Twilights';
+console.log(movieOfTheYear);
+const hobby = 'fishing';
+console.log(hobby);
+const color = 'black';
+console.log(color);
+
+
+
+
+
+
