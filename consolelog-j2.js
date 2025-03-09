9530176e4a5e950f0cb8beed6f1c225e3e1262f9
devjahid1 <developerjahid1@gmail.com>
2025-03-10 03:20:17 +0600

    consolelog done

---
+++ consolelog-j2.js
@@ -0,0 +1,20 @@
+const age = 22;
+console.log(age);
+const booksToRead = 4;
+console.log(booksToRead);
+const wakeUpTime = 7;
+console.log(wakeUpTime);
+const wastedTime = 4;
+console.log(wastedTime);
+const borrow = 50;
+console.log(borrow);
+const goal = 9;
+console.log(goal);
+const pillow = 2;
+console.log(pillow);
+
+
+
+
+
+
