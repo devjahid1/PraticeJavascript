1bd69fadc8d980880e289fea756908f71561548c
devjahid1 <developerjahid1@gmail.com>
2025-03-11 05:48:03 +0600

    typeof done

---
+++ type-j5.js
@@ -0,0 +1,14 @@
+const price = 555;
+console.log(typeof price);
+const learning = true;
+console.log(typeof learning);
+const megaCity = "Dhaka";
+console.log(typeof megaCity);
+const temperature = 102.5;
+console.log(typeof temperature);
+const isHungry = true;
+console.log(typeof true);
+const isRice = true;
+console.log(typeof true);
+
+
