1aa24668784c990d3f0a326dabeff2f4b90e8340
devjahid1 <developerjahid1@gmail.com>
2025-03-11 06:01:35 +0600

    const-let done

--- const-let-j6.js
+++ const-let-j6.js
@@ -2,3 +2,35 @@
 let age = 21;
 age = 22;
 console.log(age);
+//2
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+//3
+let studyTime = 4;
+studyTime = 0;
+console.log(studyTime);
+//4
+let friendCount = 4;
+friendCount = 2;
+console.log(friendCount);
+//5
+let phonePrice = 25000;
+phonePrice = 12000
+console.log(phonePrice);
+//6
+let chairsCount = 40;
+chairsCount = 40-2;
+console.log(chairsCount);
+chairsCount = 38-6;
+console.log(chairsCount);
+//7
+let playTime = 3;
+playTime = 0;
+console.log(playTime);
+
+
+
+
+
+
