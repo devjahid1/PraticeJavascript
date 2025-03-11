f5ef65a7d0fc9fe15ffea502bc963d61b6d5ff72
devjahid1 <developerjahid1@gmail.com>
2025-03-11 06:14:09 +0600

    comment-j7.js

---
+++ comment-j7.js
@@ -0,0 +1,54 @@
+//1
+//Cricket Score
+let score = 10;
+console.log(score);
+//2
+//this is border radius
+//this is borders radius
+//This is BOrder Radius
+let radius = 34;
+console.log(radius);
+//3
+/**
+ * this
+ * is
+ * my 
+ * fevorate
+ * dress
+ */
+let dress = "shirt";
+console.log(dress);
+//4
+//My Salary
+let salary = 100;
+console.log(salary);
+//5
+/**
+ * bangla
+ * english
+ * math
+ * System Design
+ */
+let studyTime = 7;
+console.log(studyTime);
+//6
+/**
+ * total sent 1000 taka
+ * total received 980 taka
+ */
+let sendMoney = 1000;
+console.log(1000);
+//7
+/**
+ * product price = 200 taka
+ * delivery fee = 22 taka
+ */
+
+let pay = 200+ 22;
+console.log(pay);
+
+
+
+
+
+
