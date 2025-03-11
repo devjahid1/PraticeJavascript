fb5ae94e976f724f5a2c108bb62ddd67055ba10c
devjahid1 <developerjahid1@gmail.com>
2025-03-12 02:57:43 +0600

    case done

---
+++ case-j8.js
@@ -0,0 +1,33 @@
+//1
+let favoriteFood = "Mango";
+console.log(favoriteFood);
+//2
+let city = "Chittagong";
+let City = "Dhaka";
+console.log(city);
+console.log(City);
+//3
+let userName = "Johan";
+let username = "jovan";
+console.log(userName);
+console.log(username);
+//4
+let RustomPur = "Nabiganj";
+console.log(RustomPur);
+//5
+let super = 40;
+console.log(super);//error because super is a reserve keyword
+//6
+let class = 20;
+console.log(class);//error because class is a reserve keyword
+//7
+//we can't use number first declare variable name
+let 2ndPosition = "Masraffe";
+console.log(2ndPosition);
+
+
+
+
+
+
+
