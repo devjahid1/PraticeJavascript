62e3ea8a005418d25dc6e106b8c5fdcc719361fb
devjahid1 <developerjahid1@gmail.com>
2025-03-14 02:20:12 +0600

    comparison-j10

---
+++ comparison-j10.js
@@ -0,0 +1,35 @@
+//1
+let boroBhai = 7;
+let chotoBhai = 5;
+console.log(boroBhai>chotoBhai);
+//2
+let myClass = 45;
+let otherClass = 50;
+console.log(myClass>otherClass);
+//3
+let me = 75;
+let myFriend = 75;
+console.log(me === myFriend);
+//4
+let savingsTarget = 100;
+let totalSavings = 95;
+console.log(savingsTarget<=totalSavings);
+//5
+let num1 = 10;
+let num2 = 7;
+console.log(num1 === num2);
+//6
+let number1 = 20;
+let number2 = 15;
+console.log(number1<=number2);
+//7
+let myNum = 45;
+let friendNum = 97;
+console.log(myNum>friendNum);
+
+
+
+
+
+
+
