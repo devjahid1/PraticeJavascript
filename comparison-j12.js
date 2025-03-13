ab153b54469cdcb20e5b50c6835d52222e2d8d45
devjahid1 <developerjahid1@gmail.com>
2025-03-14 03:01:00 +0600

    comparison-j12 done

---
+++ comparison-j12.js
@@ -0,0 +1,39 @@
+//1
+let chotoBhai = 10;
+let boroBhai = 15;
+console.log(chotoBhai<=boroBhai);
+//2
+let = passMark = 33;
+let = achiveMark = 45;
+console.log(passMark<achiveMark);
+//3
+let jambura = 30;
+let kodbell = 20;
+console.log(jambura === kodbell);
+//4
+let me = 10;
+let friend = 10;
+console.log(me === friend);
+//5
+let myBis = 15;
+let friendBis = 20;
+console.log(myBis ==friendBis);
+//6
+let madrid = 2;
+let barsha = 3;
+console.log(madrid == barsha);
+//7
+let myName = "Rahim";
+let friendName = "Karim";
+console.log(myName == friendName );
+//8
+let t = true;
+let f = false;
+console.log(t === f);
+
+
+
+
+
+
+
