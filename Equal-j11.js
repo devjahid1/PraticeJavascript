a4a3a3b5f6f97b97c903d2b0b7c88d786fed32c8
devjahid1 <developerjahid1@gmail.com>
2025-03-14 02:36:26 +0600

    equal done

---
+++ Equal-j11.js
@@ -0,0 +1,54 @@
+//1
+let me = 50;
+let friend = "50";
+console.log(me==friend);
+console.log(me === friend);
+//2
+let js1 = "Javascript";
+let js2 = 'Javascript';
+console.log(js1==js2);
+console.log(js1===js2);
+/**
+ * js1 & js2 there are string no matter the quote. 
+ * we can use double quote or single quote declare string
+ */
+//3
+let num1 = 25;
+let num2 = "25";
+console.log(num1 == num2);
+console.log(num1 === num2);
+/**
+ * num1 is a number and num2 is a string. if we use double equal then is didn't compare data type.
+ * if we use triple equal then it check data type
+ */
+//4
+let apple1 = "apple";
+let apple2 = "apple";
+console.log(apple1 == apple2);
+console.log(apple1 === apple2);
+//5
+let test1 = "test";
+let test2 = "TEST"
+console.log(test1 == test2);
+console.log(test1 === test2);
+//6
+let variableX = 15;
+let variableY = "20";
+console.log(variableX!= variableY);
+console.log(variableX!== variableY);
+//7
+let variableA = hello;
+let variableB = "Hello";
+console.log(variableA == variableB);
+console.log(variableA === variableB);
+/**
+ * Error Asbe
+ */
+
+
+
+
+
+
+
+
