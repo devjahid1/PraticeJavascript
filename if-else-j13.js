80c23eb12cee8455a9326d90a12e828c56ca941b
devjahid1 <developerjahid1@gmail.com>
2025-03-15 02:33:53 +0600

    if-else

---
+++ if-else-j13.js
@@ -0,0 +1,46 @@
+//1
+let returnHome = 6;
+if(returnHome <= 6){
+    console.log("Amma Nasta Kawaw");
+}
+    else{
+        console.log("6 tar pore bashay ay jaru diya oitamu");  
+    }
+//2
+let login = "login";
+if(login === "login"){
+    console.log("Welcome");
+}else{
+    console.log("Get Lost");
+    
+}
+//3
+let run = 5;
+if(run === 5){
+    console.log("Chocolate kawabe");
+}else{
+    console.log("tor buri barbe");
+    
+}
+//4
+let marks = 41;
+if(marks > 40){
+    console.log("Abba bike daw");
+}else{
+    console.log("ei sor kiser bike");
+}
+//5
+let movieTime = 8;
+if(movieTime < 9){
+    console.log("Movie dekbo");
+}else{
+    console.log("nake a tell diya ghumai");
+}
+//6
+let temp = 29;
+if(temp >= 30){
+    console.log("Start AC");
+}else{
+    console.log("Kombol diya ghumai");
+}
+
