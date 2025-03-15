c1c935cf9511f06bb58656d9f21b6623a3b70db2
devjahid1 <developerjahid1@gmail.com>
2025-03-16 02:27:50 +0600

    ternary done

---
+++ ternary-j16.js
@@ -0,0 +1,29 @@
+//1
+let cashback = 2000;
+cashback > 3000 ?console.log("Cashback: 500/-"):console.log("cashback: 0/-");
+//2
+let age = 14;
+age < 15 ? console.log("Teenager"):console.log("Child");
+//3
+let isLoggedIn = false;
+isLoggedIn === true ? console.log("Welcome Back") : console.log("Please Login");
+//4
+let isOilTankFull = false;
+isOilTankFull === true ? console.log("Ready for a long drive") : console.log("Fill the tank");
+//5
+let isExamPass = true;
+isExamPass === true ? console.log("Party Time") : console.log("Next Semester e serious Study korbo");
+;
+//6
+let sunny = false;
+sunny === false ? console.log("Stay Home"): console.log();
+//7
+let Expensive = false;
+Expensive === false ? console.log("Will buy this item") : console.log();
+;
+
+
+
+
+
+ 
