c0af53426a3843da14faca91d44892d7d3d9814d
devjahid1 <developerjahid1@gmail.com>
2025-03-15 22:12:08 +0600

    and-or done

---
+++ and-or-j14.js
@@ -0,0 +1,75 @@
+//1
+let age = 19;
+let height = 61;
+if(age > 18 && height >=60){
+    console.log("ghari telbe na, garite bose takbe");
+}else{
+    console.log("Ghari tel");
+}
+//2
+let math = 81;
+let english = 86;
+if(math > 80 && english > 85){
+    console.log("Borti howar joggo");
+}else{
+    console.log("toke biye diye dibo"); 
+}
+//3
+let gpa = 5;
+let income = 10000;
+if(gpa === 5 && income < 10000){
+    console.log("Scholarship");
+}else{
+    console.log("Fee diye porte hobe"); 
+}
+//4
+let jobAge = 30;
+let experience = 2;
+if(jobAge < 30 && experience > 2){
+    console.log("Job exam dite parbe");
+}else{
+    console.log("Job exam dite parbe na"); 
+}
+//5
+let egg = 12;
+let chicken  = 1;
+if(egg > 12 || chicken > 0){
+    console.log("Chicken kurma");
+}else{
+    console.log("pawroti and banana");
+}
+//6
+let temp = 100;
+if(temp > 100){
+    console.log("Doctor er kache jabe");
+}else{
+    console.log("kata muri diye chuye takbe"); 
+}
+//7
+let homeWork = 40;
+if(homeWork > 40){
+    console.log("exam dite parbe");
+}else{
+    console.log("Auto fail"); 
+}
+//8
+let currentNai = false;
+let mobileChargeNai = false;
+if(currentNai === true && mobileChargeNai === true){
+    console.log("Porte bosbo na");
+}else{
+    console.log("Game kelbo"); 
+}
+//9
+let productPrice = 2000; 
+let coupon = 20;
+if(productPrice > 1000 && coupon === 20){
+    console.log((productPrice*coupon)/100);
+}else{
+    console.log("kuno discount nai"); 
+}
+
+
+
+
+
