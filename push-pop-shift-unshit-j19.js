982cdae9ad5721bdc839df4f04fb81b8ba4e6508
devjahid1 <developerjahid1@gmail.com>
2025-03-17 01:56:21 +0600

    push pop done

---
+++ push-pop-shift-unshit-j19.js
@@ -0,0 +1,24 @@
+//1
+const addNumLast = [10,20,30,40,50];
+addNumLast.push(60);
+console.log(addNumLast);
+//2
+const friend = ['sojib', 'sagor', 'sakib', 'sohel'];
+friend.push('sumon');
+console.log(friend);
+//3
+const game = ['PUBG', 'Tample Run', 'Candy Crush', 'Free Fire'];
+game.pop('Free fire');
+console.log(game);
+//4
+const addNumFirst =[24, 36, 48, 48];
+addNumFirst.unshift(12);
+console.log(addNumFirst);
+//5
+const removeFirstBook = ['Bangla', 'English', 'Math', 'Physics', 'Chemistry'];
+removeFirstBook.shift('Bangla');
+console.log(removeFirstBook);
+
+
+
+
