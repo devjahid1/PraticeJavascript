876326c4e66cb52213d3ec90be3b7154cb57519d
devjahid1 <developerjahid1@gmail.com>
2025-03-21 07:08:55 +0600

    while done

---
+++ while-loop-j23.js
@@ -0,0 +1,39 @@
+//1
+let num = 50;
+while(num <= 100){
+    console.log(num); 
+    num++;
+}
+//2
+let n = 5;
+let sum = 0;
+while(n <= 15){
+    sum+=n;
+    n++
+}
+console.log(sum);
+//3
+let students = 1;
+let result = 0;
+while(students <= 50){
+    result+= students;
+    students++;
+}
+console.log(result);
+//4
+let i = 21;
+let plus = 0;
+while(i <= 50){
+    plus+=i;
+    console.log(plus);
+    i++;
+}
+//5
+let numbers = 20;
+let r = 0;
+while(numbers <= 40){
+    r+= numbers;
+    numbers++;
+}
+console.log(r);
+
