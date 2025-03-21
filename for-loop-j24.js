5187c3057bd30117ca2c97268305c2e35cae991f
devjahid1 <developerjahid1@gmail.com>
2025-03-21 07:38:19 +0600

    for

---
+++ for-loop-j24.js
@@ -0,0 +1,17 @@
+//1
+for(let i = 150; i<=170; i++){
+    console.log(i);
+}
+//2
+let r = 0
+for(let i = 31; i<=58; i++){
+    r+= i;
+}
+console.log(r);
+//3
+
+let sum = 0;
+for(let num = 25; num <= 75; num++){
+    sum+=num;
+}
+console.log(sum);
