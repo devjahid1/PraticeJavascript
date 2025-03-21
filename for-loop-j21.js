dea17e15ef47859e35b9077bf768a83690cc990e
devjahid1 <developerjahid1@gmail.com>
2025-03-21 06:48:06 +0600

    for-loop done

---
+++ for-loop-j21.js
@@ -0,0 +1,26 @@
+//1
+const books = ["Bangla", "English", "Math", "Biology", "Chemistry"];
+for(const book of books){
+    console.log(book); 
+}
+//2
+const foods = ["Pizza", "Burger", "Sandwich", "corn", "Coffee"];
+for(const food of foods){
+    console.log(food);
+}
+//3
+const birthDay = ["1970", "1985", "2002", "2005", "2008"];
+for(const birth of birthDay){
+    console.log(birth);
+}
+//4
+const players = ["Messi", "Neymar", "Rolando", "Dimaria", "Hamza", "Maradona", "Kaka", "Martunez", "paul", "Almada", "Enzo"];
+for(const player of players){
+    console.log(player);
+}
+//5
+const nextDate = ["10 tarik", "13 tarik", "15 Tarik", "18 tarik", "line breakdown"];
+for(const date of nextDate){
+    console.log(date);
+    
+}
