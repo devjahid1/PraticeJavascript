a5fbc603843a72741f92120e9c52902aadba2b68
devjahid1 <developerjahid1@gmail.com>
2025-03-21 07:25:24 +0600

    while-loop-j22 done

---
+++ while-loop-j22.js
@@ -0,0 +1,35 @@
+//1
+let i = 1;
+while(i <= 10){
+    console.log("Ami protidin 3 gonta programming pratice korbo");
+    i++
+}
+//2
+let num = 1;
+while(num <= 20){
+    console.log(num);
+    num++;
+}
+//3
+let num1 = 50;
+while(num1 <= 100){
+    console.log(num1);
+    num1++;
+}
+//4
+let namata = 1;
+let result = 3;
+while(namata <= 10){
+   let n = result * namata
+    console.log(n);
+    namata++
+}
+//4
+let divide = 111;
+let r = 2;
+while( divide >= 110){
+    let div = r / divide;
+    console.log(div);
+    divide--;
+}
+
