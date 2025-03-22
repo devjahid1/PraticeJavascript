67d4243575e393dc38d3aa088b9fbd64f10c3a2b
devjahid1 <developerjahid1@gmail.com>
2025-03-23 03:49:23 +0600

    obj-j26 done

---
+++ object-j27.js
@@ -0,0 +1,57 @@
+//1
+const teacher = {
+    name: "Anwara",
+    subject: "OOP",
+    age: 27,
+    Address: "Modina Market",
+    rating: 4.8
+}
+console.log(teacher);
+//2
+const tree = {
+    name:"Mango Tree",
+    height: 25 +" Feet",
+    age: 10 + "Years",
+    fruit: "Mango"
+}
+console.log(tree);
+//3
+const animal = {
+    anim1: "cow",
+    anim2: "Dog",
+    animal3: "ant"
+}
+console.log(animal);
+//4
+const father = {
+    name: "Saad Miah",
+    passion: "Business",
+    age: "54",
+    Total_Child: 3
+}
+console.log(father);
+//5
+ const motorBike ={
+    brandName: "Yamaha",
+    totalTire: 2,
+    color: "black",
+    speed: "160",
+    price: 270000
+ }
+ //6
+ const bird={
+    name: "panguin",
+    color: "black",
+    eat: "krill"
+ }
+console.log(bird);
+//7
+const laptop={
+    name: "Dell",
+    processor: "intel core i7",
+    price: 140000,
+    display_size: 16 + "Inch"
+}
+console.log(laptop);
+
+
