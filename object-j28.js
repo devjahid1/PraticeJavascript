fc942599af665df54abc6446b6d1ea5be5d1279a
devjahid1 <developerjahid1@gmail.com>
2025-03-23 14:15:43 +0600

    object-j28 done

---
+++ object-j28.js
@@ -0,0 +1,68 @@
+//1
+const player ={
+    name: "Messi",
+    age: 36,
+    sports: "Football",
+    team: "Argentina"
+}
+console.log(player.team);
+//2
+const laptop = {
+    brand: "Dell",
+    price: "140000",
+    hardDisk: 1+" TB",
+    ram: 12+ " GB",
+    screenSize: 16
+}
+console.log(laptop.screenSize);
+//3
+const favPlace ={
+    name: "Cox's Bazar",
+    distance: 400+" km",
+    popularity: "High"
+}
+console.log(favPlace['popularity']);
+//4
+const phone = {
+    brand:"Nokia",
+    color:"Black",
+    price:5000,
+}
+console.log(phone['price']);
+//5
+const library ={
+    name: "Public Library",
+    location: "Dhaka",
+    books: 5000
+}
+console.log(library.location);
+//6
+const movie ={
+    title: "Inception",
+    director: "Nolan",
+    rating: 9
+}
+console.log(movie['rating']);
+//7
+const collage ={
+    name: "NDC",
+    established: 1949,
+    groups: ['Science', 'Arts', 'Commerce']
+}
+console.log(collage.groups[1]);
+//8
+const family ={
+    father:{
+        name:"Saad Miah",
+        age: 54,
+        profession: "Businessman"
+    },
+    mother:{
+        name:"Saleha Begum",
+        age: 37,
+        profession: "House Wife"
+    }
+}
+console.log(family.father.age + family.mother.age);
+
+
