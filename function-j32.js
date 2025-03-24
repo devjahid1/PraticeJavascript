5bd559eba814600aaeeb847b401bf9c33fb57d90
devjahid1 <developerjahid1@gmail.com>
2025-03-25 01:14:45 +0600

    function-j32

---
+++ function-j32.js
@@ -0,0 +1,57 @@
+//1
+function total (father, son){
+    return father + son
+}
+const result = total(54, 22);
+console.log(result);
+//2
+function cal(a, b){
+    return a * b
+}
+const res = cal(20, 15);
+console.log(res);
+//3
+function marks(bangla, english, math){
+    return bangla + english + math
+}
+const totalMarks= marks(92, 97, 91);
+console.log(totalMarks);
+//4
+function birthYear(birthYear, currentYear){
+    return currentYear - birthYear
+}
+const years = birthYear(2002, 2025)
+console.log(years);
+//5
+function lau(amount, lauPrice){
+    return amount / lauPrice;
+}
+const lauPrice = lau(205, 35);
+console.log(lauPrice);
+//6
+function average(a, b, c, d, avg){
+    const plus = a+b+c+d;
+    return plus / avg
+}
+const avg = average(10, 20, 30, 40, 4);
+console.log(avg);
+//7
+function sell(parches, sellingPrice){
+    return parches + sellingPrice
+}
+const selling = sell(300, 250);
+console.log(selling);
+//8
+function year (birthYear){
+    return birthYear + 100;
+}
+const birthYears = year(2002);
+console.log(birthYears);
+//9
+function useMobile(hours){
+    return hours * 30
+}
+const mobileUse = useMobile(4);
+console.log(mobileUse);
+
+
