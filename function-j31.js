17eab51fc24b1f69fa7d90974acd8f0377216c43
devjahid1 <developerjahid1@gmail.com>
2025-03-25 00:16:47 +0600

    function-j31.js done

---
+++ function-j31.js
@@ -0,0 +1,47 @@
+//1
+function juiceKaw(){
+    console.log("Ei naw lebur Juice"); 
+}
+juiceKaw();
+//2
+function  myRoutine(){
+    console.log("Wake up, brush teeth, exercise, take a shower, have breakfast, start work."); 
+}
+myRoutine();
+//3
+function familyMembers(name) {
+    console.log(name);
+}
+//3
+function familyMembers() {
+    console.log("Jahid");
+    console.log("Hasib");
+    console.log("Sakib");
+}
+familyMembers();
+//4
+function taskDone(){
+    console.log("Exercise done");
+    console.log("Exercise done");
+    console.log("shower done");
+    console.log("Breakfast done");
+    console.log("Programming Practice done");
+}
+taskDone();
+//5
+function greetMe(){
+    console.log("Good Morning! Have a great day");
+}
+greetMe();
+//6
+function getFriends(){
+    console.log("Tashin");
+    console.log("Taher");
+    console.log("Sayem");
+}
+getFriends();
+//7
+function introduceMyself(){
+    console.log("Name: Jahid \nAge: 22 \nAdrees: Sylhet \nPhone: 01701308463 \nHeight: 5'6 inch, \nFavorite Food: Kacchi");
+}
+introduceMyself()
