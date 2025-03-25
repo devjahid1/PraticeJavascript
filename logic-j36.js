5f0177b0d8154d3b68ef007eb162f38c2a8319d1
devjahid1 <developerjahid1@gmail.com>
2025-03-25 23:59:27 +0600

    logic-j36.js

---
+++ logic-j36.js
@@ -0,0 +1,41 @@
+//1
+function nums(miles){
+    return 1760 * miles;
+}
+console.log(nums(13));
+//2
+function kilo(nums){
+    return 860 * nums
+}
+console.log(kilo(5));
+//3
+function hours(hrs){
+    let sec = hrs * 3600
+    return sec;
+}
+console.log(hours(1));
+//4
+function miters(miter){
+    return miter * 1000;
+}
+console.log(miters(2));
+//5
+function inchs(inch){
+    return inch * 2.54;
+}
+console.log(inchs(5));
+//6
+function pounds(pound){
+    return pound * 0.453;
+}
+console.log(pounds(3));
+//7
+function metters(metter){
+    return metter * 0.91
+}
+console.log(metters(5));
+
+
+
+
+
