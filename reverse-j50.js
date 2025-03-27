11597e0c431e2b96ee1c7c80995f2308d6470b4a
devjahid1 <developerjahid1@gmail.com>
2025-03-28 05:18:35 +0600

    reverse-j50

---
+++ reverse-j50.js
@@ -0,0 +1,13 @@
+//1
+const names = ['Zara', 'Tariq', 'Amir', 'Lina'];
+names.sort().reverse();
+console.log(names);
+//2
+const numbers = [1, 4, 9, 7];
+numbers.reverse();
+console.log(numbers);
+//3
+const users = [{name: 'Ali', age: 30}, {name: 'Sara', age: 30}, {name: 'Tariq', age: 35}];
+users.reverse();
+console.log(users);
+
