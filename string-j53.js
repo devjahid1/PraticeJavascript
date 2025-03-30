58a333d1bf25b30e6a7f1ec3d124d4fffcbbf49c
devjahid1 <developerjahid1@gmail.com>
2025-03-31 04:56:57 +0600

    string-j53.js

---
+++ string-j53.js
@@ -0,0 +1,23 @@
+//1
+const email = 'User@Example.com';
+console.log(email.toLowerCase());
+//2
+const greeting = 'hELlo WoRLd';
+console.log(greeting.toUpperCase());
+console.log(greeting.toLowerCase());
+//3
+const language = 'JavaScript';
+const converted = language.toLowerCase();
+if(converted.includes('script')){
+console.log('script is available');
+}else{
+    console.log("Not available");
+}
+//4
+const text = "NodeJs";
+console.log(text[0] === text[0].toUpperCase())
+;
+
+
+
+
