c28528587013a380f445c678cb4821d20a8ec6ad
devjahid1 <developerjahid1@gmail.com>
2025-03-31 04:36:58 +0600

    string-j52.js

---
+++ string-j52.js
@@ -0,0 +1,22 @@
+//1
+const city = "Chattogram";
+console.log(city.indexOf("g"));
+//2
+const division = "Sylhet";
+console.log(division.includes("y"));
+//3
+const name = "Rifat";
+console.log(name[4]);
+//4
+const language = "Javascript";
+console.log(language.indexOf("rip"));
+//5
+const text = "Immutable";
+console.log(text.length);
+console.log(text[0]);
+
+
+
+
+
+
