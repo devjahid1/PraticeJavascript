7ea7af462622ada2834451f66fedda4b828b7798
devjahid1 <developerjahid1@gmail.com>
2025-04-05 05:18:12 +0600

    j57

---
+++ jscore-j57.js
@@ -0,0 +1,28 @@
+//1
+let arr = [];
+console.log(arr);
+//2
+function addNumbers(a,b){
+    console.log(a+b);
+}
+let result = addNumbers(5, 10);
+console.log(result);
+//3
+let numbers = [10,20,30,40,50];
+let index = numbers.indexOf(30);
+numbers.splice(index, 2);
+console.log(numbers);
+//4
+function sum(a,b,c){
+
+}
+let results = sum(5, 10, 15);
+console.log(results);
+//5
+let student ={
+    name: "serious sojib",
+    roll: 1,
+    marks: 99,
+}
+student.age = 20;
+console.log(student);
